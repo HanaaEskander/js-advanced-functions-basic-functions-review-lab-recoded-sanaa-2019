4e55c06be2576ba7476c6cdf18c565533e1e17d5
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-03 12:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+function dairy1(activity="Greeting"){
+  return `Tmw, I am ${activity} my students!`;
+}
+
+function dairy2(activity="attend meeting"){
+  return `Tmw, I will ${activity}.`;
+}
+function wrapAdjective(value = "*"){
+  return function(item = "special"){
+  return `You are ${value}${item}${value}!`;
+  }
+}
+
+const Calculator = {
+  add:function(arg1, arg2){
+    return arg1 + arg2;
+  },
+  subtract:function(arg1, arg2){
+    return arg1 - arg2;
+  },
+  multiply:function(arg1, arg2) {
+    return arg1 * arg2;
+  },
+  divide:function(arg1, arg2){
+    return arg1 / arg2;
+  }
+}
+function actionApplyer (start , funArr){
+    if (funArr.length === 0 ) {
+        return 0 ;
+    }
+    for (const fun of funArr) {
+        start = fun(start);
+    }
+    return start;
+}
+

6a0ee86c653e56ae3bb44a07a7105b20f3058771
Hana'a Eskander <hanaa.eskander95@gmail.com>
2019-12-03 12:44:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,25 +7,11 @@ function dairy2(activity="attend meeting"){
   return `Tmw, I will ${activity}.`;
 }
 function wrapAdjective(value = "*"){
-  return function(item = "special"){
-  return `You are ${value}${item}${value}!`;
+  return function(me = "special"){
+  return `You are ${value} ${me} ${value}!`;
   }
 }
 
-const Calculator = {
-  add:function(arg1, arg2){
-    return arg1 + arg2;
-  },
-  subtract:function(arg1, arg2){
-    return arg1 - arg2;
-  },
-  multiply:function(arg1, arg2) {
-    return arg1 * arg2;
-  },
-  divide:function(arg1, arg2){
-    return arg1 / arg2;
-  }
-}
 function actionApplyer (start , funArr){
     if (funArr.length === 0 ) {
         return 0 ;
